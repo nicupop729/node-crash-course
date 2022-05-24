dd968ad858ce64c6a3d40041e94f7db01abc415f
Nicolae Pop <nicupop729@gmail.com>
2022-05-24 16:04:06 -0400

    Lesson 10: Get a specific item

--- app.js
+++ app.js
@@ -62,7 +62,7 @@ app.use(morgan("dev"));
 
 // routes
 app.get("/", (req, res) => {
- res.redirect("/blogs");
+  res.redirect("/blogs");
 });
 
 app.get("/about", (req, res) => {
@@ -70,28 +70,39 @@ app.get("/about", (req, res) => {
 });
 
 // blog routes
-app.get('/blogs', (req, res) => {
-    Blog.find().sort({ createdAt: 'desc' })
+app.get("/blogs", (req, res) => {
+  Blog.find()
+    .sort({ createdAt: "desc" })
     .then((result) => {
-        res.render('index', { title: "All Blogs", blogs: result });
+      res.render("index", { title: "All Blogs", blogs: result });
     })
     .catch((err) => {
-        console.log(err);
+      console.log(err);
     });
 });
 
-app.post('/blogs', (req, res) => {
-    const blog = new Blog(req.body);
-    blog
+app.post("/blogs", (req, res) => {
+  const blog = new Blog(req.body);
+  blog
     .save()
     .then((result) => {
-        res.redirect('/blogs');
+      res.redirect("/blogs");
     })
     .catch((err) => {
-        console.log(err);
+      console.log(err);
     });
 });
 
+app.get("/blogs/:id", (req, res) => {
+  const id = req.params.id;
+  Blog.findById(id)
+    .then((result) => {
+      res.render("details", { blog: result, title: 'Blog Details' });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
 
 app.get("/blogs/create", (req, res) => {
   res.render("create", { title: "Create a new blog" });
